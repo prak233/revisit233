65cede7bd307d9eff6e315bc11843b09471d7a31
pratul <pratul_pkp@outlook.com>
2024-11-15 12:45:20 +0530

    First Commit

---
+++ HelloWorldapp.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloWorldapp.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
