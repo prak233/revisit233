49e2ad2e8b649b8c6b747bf254f70e79a338f0cf
pratul <pratul_pkp@outlook.com>
2024-11-16 12:24:28 +0530

    dev initial commit

--- HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
+++ HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65cede7bd307d9eff6e315bc11843b09471d7a31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+198662ea18d76da38e92be4de9e1cb2b42f6f6d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
