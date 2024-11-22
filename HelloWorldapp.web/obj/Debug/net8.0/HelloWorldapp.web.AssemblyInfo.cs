c93b947458fcbc9ffed772fbf2814e04e0f27153
pratul <pratul_pkp@outlook.com>
2024-11-22 13:20:28 +0530

    changes made to buildp & multistage

--- HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
+++ HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22cadf0521f56b7f7431af498acd2293a69e011e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9976c5c38dc5543f016b1c129b6af2f4fecfb996")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
