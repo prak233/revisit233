db598ba3a1f204e24b7c0e26fc4a657c049900e8
pratul <pratul_pkp@outlook.com>
2024-12-10 00:39:19 +0530

    Updated multistageCICDwithApprovals.yml with iis deployment

--- HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
+++ HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25355ae8a355297a9dda573d3b6fd909db3d3f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff36a854c216c8dc8a82e208f4be4585e65c709")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
