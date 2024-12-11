5650cb811194fdc455517adf13c06a50cb52b9b6
pratul <pratul_pkp@outlook.com>
2024-12-11 23:17:27 +0530

    removed pool from build template

--- HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
+++ HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d32dc6c0b49d2dd952df60e8879b6a04634b6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528f4b4370278e5d1d930310cff72226783d096e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
