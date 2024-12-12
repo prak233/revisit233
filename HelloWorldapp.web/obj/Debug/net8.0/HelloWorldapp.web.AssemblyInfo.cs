bf5453b29fd56a46d16660df15484cbcb2bf468c
pratul <pratul_pkp@outlook.com>
2024-12-13 00:49:09 +0530

    Configured revisit233-multibranching.yml for TESTRUN to TEST #1

--- HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
+++ HelloWorldapp.web/obj/Debug/net8.0/HelloWorldapp.web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ffd4ff591b51a0367fa2a8bcdd2cf12753b509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc3826ae3eb667bfa19badb0c8bd65365aef3f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldapp.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
